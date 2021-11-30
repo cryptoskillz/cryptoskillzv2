b041cb94e43e55a4e654c024ac25073874fe32e1
chris <cryptoskillz@chriss-MacBook-Air.local>
2021-11-30 14:48:12 +0700

    function

--- functions/api/name.js
+++ functions/api/name.js
@@ -1,26 +1,32 @@
 
 
+
 export async function onRequest(context) {
   // Contents of context object
   const {
     request, // same as existing Worker API
-    env, // same as existing Worker API
     params, // if filename includes [id] or [[path]]
     waitUntil, // same as ctx.waitUntil in existing Worker API
     next, // used for middleware or to fetch assets
     data, // arbitrary space for passing data between middlewares
+    env,
   } = context;
 
+
   try {
-   	const value = await CZBLOG.list()
-  	return new Response(value.keys)
+  	console.dir(env)
+    console.log(env.test)
+   	const value = await env.CZBLOG.list()
   } catch (err) {
   	//console.dir(context)
+  	  //IMAGES: KVNamespace;
+
+  	console.dir(context)
+  	//console.log(params)
+
     return new Response(err);
   }
 
-
-  
 }
 
 
