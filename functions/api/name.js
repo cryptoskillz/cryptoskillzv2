e02689e821bce4c41d955d6fe22006d3ade23a49
chris <cryptoskillz@chriss-MacBook-Air.local>
2021-12-05 17:56:03 +0700

    kv store

--- functions/api/name.js
+++ functions/api/name.js
@@ -1,18 +1,28 @@
-const errorHandler = async ({ next }) => {
-  try {
-    return await next()
+export async function onRequest(context) {
+  // Contents of context object
+  const {
+    request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+    waitUntil, // same as ctx.waitUntil in existing Worker API
+    next, // used for middleware or to fetch assets
+    data, // arbitrary space for passing data between middlewares
+  } = context;
+    try {
+      const ALLOW_TEST = (context.env.ALLOW_TEST === "True");
+      console.log(ALLOW_TEST)
+      const KV = context.env.CFKVDB;
+      console.log(KV)
+      const key = await KV.get("wah22")
+      console.log(key)
+      //console.log(context)
+      //KV.delete("wah22")
+      //KV.put("wah22", "gah",{})
+      const value = await KV.list()
+      console.log(value)
+      return new Response("Hello, world!");
   } catch (err) {
-    return new Response(`${err.message}\n${err.stack}`, { status: 500 })
+    console.log(err)
   }
-}
 
-const hello = async ({ next }) => {
-  const response = await next()
-  response.headers.set('X-Hello', 'Hello from functions Middleware!')
-  return response
 }
-
-export const onRequest = [
-  errorHandler,
-  hello
-]
