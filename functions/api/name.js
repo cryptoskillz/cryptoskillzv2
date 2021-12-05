e184314b4ee62a6aadd2455cd217bde99b4f8201
chris <cryptoskillz@chriss-MacBook-Air.local>
2021-12-05 22:23:15 +0700

    kv store

--- functions/api/name.js
+++ functions/api/name.js
@@ -1,7 +1,11 @@
 export async function onRequest(context) {
   // Contents of context object
-  /*
-  const {
+  
+
+  
+    try {
+
+        const {
     request, // same as existing Worker API
     env, // same as existing Worker API
     params, // if filename includes [id] or [[path]]
@@ -9,8 +13,8 @@ export async function onRequest(context) {
     next, // used for middleware or to fetch assets
     data, // arbitrary space for passing data between middlewares
   } = context;
-  */
-    try {
+
+
       //const ALLOW_TEST = (context.env.ALLOW_TEST === "True");
       //console.log(ALLOW_TEST)
       const KV = context.env.CFKVDB;
@@ -22,10 +26,10 @@ export async function onRequest(context) {
       //KV.put("wah22", "gah",{})
       const value = await KV.list()
       //console.log(value)
-      //console.log(context)
+      console.log(context)
       return new Response(value.list_complete);
   } catch (err) {
-      return new Response(err);
+      return new Response(context.env);
   }
 
 }
